1e4f52c580dd11f36f1a5829398426a556393b6a
Lakshyajit <lakshyajit165@gmail.com>
2019-08-28 15:32:57 +0530

    dropdown functionality complete!

--- com/stackroute/RootController.java
+++ com/stackroute/RootController.java
@@ -5,15 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class RootController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView root() {
+        String type[] = new String[]{"Cricket", "Badminton", "Football"};
+        List<String> eventType = new ArrayList<>(Arrays.asList(type));
+
         String test = "test";
         ModelAndView mv = new ModelAndView();
         mv.setViewName("index");
-        mv.addObject("name", test);
+        mv.addObject("types", eventType);
         return mv;
     }
 }
--- webapp/WEB-INF/index.jsp
+++ webapp/WEB-INF/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8" isELIgnored="false" %>
 <html>
@@ -14,9 +15,16 @@
             <label>Price: </label>
             <input type="text" name="price"><br>
 
+<%--            <label>Type: </label>--%>
+<%--            <input type="text" name="type"><br>--%>
             <label>Type: </label>
-            <input type="text" name="type"><br>
+            <select name="type">
+                <c:forEach var="type" items="${types}">
+                    <option value=${type}>${type}</option>
+                </c:forEach>
 
+
+            </select>
             <input type="submit">
         </form>
 
